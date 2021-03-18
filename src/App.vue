fffc7e2f152d07a1d450d467006aeafaad985651
Abraham <nencho_1@hotmail.com>
2021-03-18 17:36:22 +0100

    deleted fontawesome package

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Registro</router-link>
+      <router-link to="/">Registro</router-link> |
       <router-link to="/caminos">Caminos</router-link>
     </div>
     <router-view/>
@@ -9,21 +9,23 @@
 </template>
 
 <style>
-*{
-  margin: 0;
-  padding: 0;
-  border: 0;
-  box-sizing: border-box;
-  text-decoration: none;
-  list-style: none;
-}
 
-:root {
-    --primary-color: #9ccf31;
-    --secondary-color: #f7d708;
-    --color-icons: #536df6;
-    --complementary-color: #ff9e00;
-    --contact-color: #ce0000;
-    --font-family: 'Redressed', cursive;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+  }
+
+  :root {
+      --primary-color: #9ccf31;
+      --secondary-color: #f7d708;
+      --color-icons: #536df6;
+      --complementary-color: #ff9e00;
+      --contact-color: #ce0000;
+      --font-family: 'Redressed', cursive;
+  }
+
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,16 +2,19 @@
   <footer>
       <ul>
           <li>
-              <img src="../assets/profile-user.svg" alt="">
+            <img src="../assets/profile-user.svg" alt="Icono de usuario para login">
           </li>
+
           <li>
-              <img src="../assets/bed.svg" alt="">
+            <img src="../assets/bed.svg" alt="Icono de una cama">
           </li>
+          
           <li>
-              <img src="../assets/chat.svg" alt="">
+            <img src="../assets/chat.svg" alt="Icono de mensajes para chat">
           </li>
+
           <li>
-              <img src="../assets/location-pin.svg" alt="">  
+            <img src="../assets/location-pin.svg" alt="Icono de localización">  
           </li>
       </ul>
   </footer>
@@ -24,15 +27,18 @@ export default {
 </script>
 
 <style>
+
     footer{
         background-color: var(--secondary-color);
         position: absolute;
         bottom: 0;
         width: 100%;
     }
+
     footer ul{
         padding: 1.5vh;
         display: flex;
         justify-content: space-between;
     }
+
 </style>
