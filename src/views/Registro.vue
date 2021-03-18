42d870a1befbb616ee2ea807546191167fcbebd6
Luisherediaj <73532702+Luisherediaj@users.noreply.github.com>
2021-03-18 12:04:56 +0100

    Añadidos formularios login logup en view Registro

---
+++ src/views/Registro.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h1>Login</h1>
+    <figure>
+      <img src="" alt="">
+    </figure>
+
+    <form action="">
+      <label for="email">Email</label>
+        <input type="email" placeholder="Usuario">
+      <label for="password">Contraseña</label>
+        <input type="password" placeholder="Contraseña">
+      <input type="submit" value="login">
+    </form>
+
+    <h1>Logup</h1>
+    <form action="">
+      <label for="nombre">Nombre</label>
+        <input type="text" placeholder="Nombre">
+      <label for="apellidos">Apellidos</label>
+        <input type="text" placeholder="Apellidos">
+      <label for="nacionalidad">Nacionalidad</label>
+        <input type="text" placeholder="Nacionalidad">
+      <label for="email">Email</label>
+        <input type="email" placeholder="Email">
+      <label for="password">Contraseña</label>
+        <input type="password" placeholder="Contraseña">
+      <input type="submit" value="logup">
+    </form>
+  </main>
+</template>
+
