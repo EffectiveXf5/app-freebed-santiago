9ecff146ffeba34265a49d632eb58f24ecd9d4a2
Abraham <nencho_1@hotmail.com>
2021-03-18 14:37:17 +0100

    added icon

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -8,8 +8,10 @@
       <form>
         <label>Email</label>
         <input type="email" placeholder="Usuario...">
+
         <label>Contraseña</label>
         <input type="password" placeholder="Contraseña...">
+
         <input type="submit" value="login">
       </form>
     </section>
@@ -18,14 +20,19 @@
       <form>
         <label>Nombre</label>
         <input type="text" placeholder="Nombre...">
+
         <label>Apellidos</label>
         <input type="text" placeholder="Apellidos">
+
         <label>Nacionalidad</label>
         <input type="text" placeholder="Nacionalidad...">
+
         <label>Email</label>
         <input type="email" placeholder="Email...">
+
         <label>Contraseña</label>
         <input type="password" placeholder="Contraseña...">
+        
         <input type="submit" value="logup">
       </form>
     </section>
