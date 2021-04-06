d302446b6359642bc7a129f3fb200e7c5bd55c1a
Abraham <nencho_1@hotmail.com>
2021-04-06 11:54:27 +0200

    finished login and register

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,24 @@
 
 <style>
 
-  *{
+  * {
     margin: 0;
     padding: 0;
     border: 0;
     box-sizing: border-box;
     text-decoration: none;
     list-style: none;
+    background-color: var(--secondary-color);
   }
 
   :root {
       --primary-color: #C1545A;
       --secondary-color: #F6F6F6;
       --color-icons: #C1545A;
+      --color-button: #DFBFC0;
       --complementary-color: #B8B5FF;
       --contact-color: #DFBFC0;
+      --input-color: #E3E3E3;
       --border-contact-color: #C1545A;
       --font-family: 'Redressed', cursive;
   }
