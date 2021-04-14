5b8941be55a89eb9a2b567224e886a0478c9125c
Abraham <nencho_1@hotmail.com>
2021-04-14 12:01:04 +0200

    finished view of google map, we have to register to use it

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,14 +1,26 @@
 <template>
   <main>
-      <h1>Tu estas aqui:</h1>
-      <p>{{ coordinates.lat }} Latitude, {{ coordinates.lng }} Longitude</p>
-      <GmapMap
-        :center="{lat:10, lng:10}"
-        :zoom="7"
+    <section>
+        <div>
+            <h1>Tus coordenadas:</h1>
+            <p>{{ myCoordinates.lat }} Latitude, {{ myCoordinates.lng }} Longitude</p>
+        </div>
+
+        <div>
+            <h1>Cordenadas del mapa:</h1>
+            <p>{{ mapCoordinates.lat }} Latitude, {{ mapCoordinates.lng }} Longitude</p>
+        </div>
+    </section>
+
+    <GmapMap
+        :center="myCoordinates"
+        :zoom="zoom"
         map-type-id="terrain"
-        style="width:640px; height:360px;"
-      >
-      </GmapMap>
+        style="width:90vw; height:90vh;"
+        ref="mapRef"
+        @dragend="handleDrag"
+    >
+    </GmapMap>
   </main>
 </template>
 
@@ -16,7 +28,9 @@
 export default {
     data() {
         return {
-            coordinates: {
+            map: null,
+
+            myCoordinates: {
                 lat: 0,
                 lng: 0
             }
@@ -24,14 +38,71 @@ export default {
     },
 
     created() {
-        this.$getLocation({})
+        
+        if(localStorage.center) {
+            this.myCoordinates = JSON.parse(localStorage.center);
+        } else {
+            this.$getLocation({})
             .then(coordinates => {
-                this.coordinates = coordinates;
+                this.myCoordinates = coordinates;
             });
+        }
+
+        if(localStorage.zoom) {
+            this.zoom = parseInt(localStorage.zoom);
+        }
+    },
+
+    mounted() {
+        this.$refs.mapRef.$mapPromise.then( map => this.map = map);
+    },
+
+    methods: {
+        handleDrag() {
+            let center = {
+                lat: this.map.getCenter().lat(),
+                lng: this.map.getCenter().lng()
+            };
+
+            let zoom = this.map.getZoom();
+
+            localStorage.center = JSON.stringify(center);
+            localStorage.zoom = zoom;
+        }
+    },
+
+    computed: {
+        mapCoordinates() {
+            if(!this.map) {
+                return {
+                    lat: 0,
+                    lng: 0
+                };
+            }
+
+            return {
+                lat: this.map.getCenter().lat().toFixed(4),
+                lng: this.map.getCenter().lng().toFixed(4)
+            };
+        }
     }
 }
 </script>
 
-<style>
+<style scoped>
+
+    main {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    main section {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
 
 </style>
